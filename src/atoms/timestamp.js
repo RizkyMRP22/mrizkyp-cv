9c8d43eaacd9bd66eaebd351c307d5ba35478a0b
RizkyMRP22 <rizkymrp22@gmail.com>
2025-04-10 16:16:29 +0700

    Updates

--- src/atoms/timestamp.js
+++
@@ -1,9 +0,0 @@
-// Sets access time in the top-left corner
-document.addEventListener("DOMContentLoaded", () => {
-    const accessTimeElement = document.getElementById("access-time");
-    if (accessTimeElement) {
-      const now = new Date();
-      const localTime = now.toLocaleString("en-GB", { hour12: false });
-      accessTimeElement.textContent = `CV Accessed: ${localTime}`;
-    }
-  });
