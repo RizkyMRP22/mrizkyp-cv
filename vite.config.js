df052e0380794ed928fef3f78a5773c50b924bd5
RizkyMRP22 <rizkymrp22@gmail.com>
2025-04-10 16:36:09 +0700

    update config branch tag

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import { execSync } from 'child_process';
 
 const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: '/mrizkyp-cv/', // ← This is important for GitHub Pages
-  define: {
-    __GIT_BRANCH__: JSON.stringify(currentBranch),
-  },
+  base: isProduction ? '/mrizkyp-cv/' : './',
+  define: !isProduction
+    ? {
+        __GIT_BRANCH__: JSON.stringify(currentBranch),
+      }
+    : {},
 });
