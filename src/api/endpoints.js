bc6ab2d840604024244953b33f0ca6a2035bb616
RizkyMRP22 <rizkymrp22@gmail.com>
2025-04-11 08:14:57 +0700

    integrate api

---
+++ src/api/endpoints.js
@@ -0,0 +1,6 @@
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://8dc89cd8-e2f7-4444-ad1f-764405691864.mock.pstmn.io';
+
+export const ENDPOINTS = {
+  PROFILE_DETAIL: '/profile-detail',
+  // Add more paths like: PROJECTS: '/projects', etc.
+};
